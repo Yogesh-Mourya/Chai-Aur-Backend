8dd57309f9674ae8bba2d186e3ef9130cd6a75dd
Yogesh <yashmourya541@gmail.com>
2025-10-01 14:32:57 +0530

    Till Lecture No.4 (i.e Date Models)

---
+++ models/todos/user.models.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema(
+    {
+        username: { 
+            type: String, 
+            required: true, 
+            lowercase: true
+        },
+        email: { 
+            type: String, 
+            required: true, 
+            unique: true,
+            lowercase: true
+        },
+        password: { 
+            type: String, 
+            required: [true, "Please provide a password"]
+        }
+    }, 
+    { 
+        timestamps: true 
+    }
+)
+
+export const User = mongoose.model("User", userSchema)
